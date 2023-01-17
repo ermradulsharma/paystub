698f1dca4de2c771710ca3134edab37fb922dabb
Mradul Sharma <mradul@possibilitysolutions.com>
2023-01-17 11:06:58 +0530

    usa

--- resources/views/usa.blade.php
+++ resources/views/usa.blade.php
@@ -1,18 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- Modal Start -->
-    <div class="modal fade" id="openEye" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <img src=" " class="setImage w-100">
-                </div>
+<!-- Modal Start -->
+<div class="modal fade" id="openEye" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src=" " class="setImage w-100">
             </div>
         </div>
     </div>
@@ -35,25 +34,21 @@
                             <input type="tel" id="tel" name="tel" placeholder="123-234-4565" class="w-100 p-2 text-center textInputFontSize">
                         </div>
 
-                            <div class="col-md-6 mt-1">
-                                <label for="tel" class="lable">EMPLOYER TELEPHONE NUMBER <span
-                                        class="redColor">*</span>
-                                </label>
-                                <input type="tel" id="tel" name="tel" placeholder="123-234-4565"
-                                    class="w-100 p-2 text-center textInputFontSize">
-                            </div>
+                    </div>
 
                     <div class="row mb-3">
                         <div class="col-md-12">
                             <label for="address_1" class="lable">STREET ADDRESS 1 <span class="redColor">*</span> </label>
                             <input type="text" id="address_1" name="address_1" placeholder="Your Employer Address" class="w-100 p-2  textInputFontSize">
                         </div>
+                    </div>
 
                     <div class="row mb-3">
                         <div class="col-md-12">
                             <label for="address_2" class="lable">STREET ADDRESS 2 <span class="redColor">*</span> </label>
                             <input type="text" id="address_2" name="address_2" placeholder="Suite 101 or Apt 101 (optional)" class="w-100 p-2  textInputFontSize">
                         </div>
+                    </div>
 
                     <div class="row mb-3">
                         <div class="col-md-4">
@@ -89,13 +84,13 @@
                                         <option selected=""> --- Select Basic Templates --- </option>
                                         @foreach ($basicType as $data)
                                         <option value="{{$data->title ?? ''}}" data-src="{{$data->images->file ?? ''}}">
-                                            {{$data->title}}
-                                        </option>
+                                            {{$data->title}}</option>
                                         @endforeach
                                     </select>
                                     <i data-src="{{$data->images->file ?? ''}}" class="fa fa-eye-slash basicTem" style="font-size: 39px;" role="button"></i>
                                 </div>
                             </div>
+                        </div>
 
                         <div class="col-md-2  text-center sh">
                             <img src="images/hrpng.png" style="height: 200px;">
@@ -109,8 +104,7 @@
                                         <option selected=""> --- Select Advance Template --- </option>
                                         @foreach ($advanceType as $data)
                                         <option value="{{$data->title ?? ''}}" data-src="{{$data->images->file ?? ''}}">
-                                            {{$data->title ?? ''}}
-                                        </option>
+                                            {{$data->title ?? ''}}</option>
                                         @endforeach
                                     </select>
                                     <i data-src="{{$data->images->file ?? ''}}" class="fa fa-eye-slash advanceTem" role="button" style="font-size: 39px;"></i>
@@ -152,12 +146,14 @@
                             <label for="fname" class="lable">STREET 1 <span class="redColor">*</span></label>
                             <input type="text" id="fname" name="fname" placeholder="Your Address" class="w-100 p-2  textInputFontSize">
                         </div>
+                    </div>
 
                     <div class="row mb-3">
                         <div class="col-md-12">
                             <label for="fname" class="lable">STREET 2 <span class="redColor">*</span> </label>
                             <input type="text" id="fname" name="fname" placeholder="Suite 101 or Apt 101(optional)" class="w-100 p-2  textInputFontSize">
                         </div>
+                    </div>
 
                     <div class="row mb-3">
                         <div class="col-md-4">
@@ -201,6 +197,7 @@
                                     <option value="audi">Audi</option>
                                 </select>
                             </div>
+                        </div>
 
                         <div class="col-md-3 mt-4">
                             <label for="fname" class="lable">AUTO CALCULATOR <span class="redColor">*</span> </label>
@@ -308,6 +305,12 @@
                         </div>
                     </div>
 
+                    <div class="row ">
+                        <div class="col-md-2 ">
+                            <button class="statementbtn">EARNING</button>
+                        </div>
+                        <div class="col-md-2 ">
+                            <button class="statementbtn">RATE</button>
                         </div>
                         <div class="col-md-2 ">
                             <button class="statementbtn">HOURS</button>
@@ -363,13 +366,18 @@
                         <div class="col-md-2 mt-2 mb-5">
                             <button class="add_button earnbtn"><i class="fa fa-plus-circle pr-2" style="font-size:24px;color:green"></i>Add Earning</button>
                         </div>
-                        <div class="field_wrapper"> </div>
+                    </div>
+                    {{-- <div class="field_wrapper"> </div> --}}
 
-                        <div class="row mb-3">
+                    {{-- <div class="row mb-3">
                             <div class="col-md-2 mt-2 mb-5">
-                                <button class="add_button earnbtn"><i class="fa fa-plus-circle pr-2"
-                                        style="font-size:24px;color:green"></i>Add Earning</button>
+                                <button class="add_button earnbtn"><i class="fa fa-plus-circle pr-2" style="font-size:24px;color:green"></i>Add Earning</button>
                             </div>
+                        </div> --}}
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <button class="createbtn ">DEDUCTIONS</button>
                         </div>
                     </div>
 
@@ -397,36 +405,29 @@
                             <button class="add_deduction earnbtn"><i class="fa fa-plus-circle pr-5" style="font-size:24px;color:green"></i>Add Deduction</button>
                         </div>
 
-                        @foreach ($dedutions as $item)
-                            <div class="row mb-3 mt-4">
-                                <div class="col-md-4 col-lg-3">
-                                    <i class="fa fa-lock earnbtn2"></i>
-                                    <input class="earnbtn text-center" type="text" value="{{ $item->title }}">
-                                </div>
-                                <div class="col-md-1 col-lg-1"></div>
-                                <div class="col-md-2 col-lg-3"></div>
-                                <div class="col-md-1 col-lg-1"></div>
-                                <div class="col-md-2 col-lg-2">
-                                    <input type="number" step="0.01" class="earnbtn text-center" value="" />
-                                </div>
-                                <div class="col-md-2 col-lg-2">
-                                    <input type="number" step="0.01" class="earnbtn text-center" value="" />
-                                </div>
-                            </div>
-                        @endforeach
-                        <div id="add_deduction" class="my-3">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-2 col-lg-3"></div>
+                        <div class="col-md-1"></div>
+                        <div class="col-md-3"></div>
+                        <div class="col-md-3"></div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-4 col-lg-3">
+                            <input class="earnbtn text-center" type="text" value="Taxes/Deduction Tax">
                         </div>
-                        <div class="row my-3">
-                            <div class="col-md-4 col-lg-3">
-                                <button class="add_deduction earnbtn"><i class="fa fa-plus-circle pr-5"
-                                        style="font-size:24px;color:green"></i>Add Deduction</button>
-                            </div>
-
-                            <div class="col-md-1"></div>
-                            <div class="col-md-2 col-lg-3"></div>
-                            <div class="col-md-1"></div>
-                            <div class="col-md-3"></div>
-                            <div class="col-md-3"></div>
+                        <div class="col-md-1"></div>
+                        <div class="col-md-2 col-lg-3"></div>
+                        <div class="col-md-1"></div>
+                        <div class="col-md-2">
+                            <input type="number" step="0.01" class="earnbtn deduction_tax text-center" value="" />
+                        </div>
+                        <div class="col-md-2">
+                            <input type="number" step="0.01" class="earnbtn ytd_deduction_tax text-center" value="" />
+                        </div>
+                    </div>
+                    <div class="row mb-3 mt-5">
+                        <div class="col-md-4 col-lg-3">
+                            <button class="netpaybtn">Net Pay</button>
                         </div>
                         <div class="col-md-1"></div>
                         <div class="col-md-2 col-lg-3"></div>
@@ -443,6 +444,7 @@
                 </div>
             </div>
         </div>
+    </div>
 
     <div>
         <h5>Template Elements</h5>
@@ -489,6 +491,8 @@
             <button class="emailbtn text-capitalize" data-toggle="modal" data-target="#myModal"> <i class="fa fa-envelope mr-4" style="font-size:24px"></i>EMAIL PAYSTUB <i class="fa fa-download ml-4" style="font-size:24px"></i></button>
         </div>
     </div>
+</div>
+
 @endsection
 @section('script')
 <script>
@@ -589,114 +593,115 @@
                 }, 300);
             });
 
-        $('.calculation').keyup(function() {
-            var id = $(this).data('id');
-            setTimeout(function() {
-                calculation(id);
-            }, 300);
+            $('.calculation').keyup(function() {
+                var id = $(this).data('id');
+                setTimeout(function() {
+                    calculation(id);
+                }, 300);
 
 
+            });
+
+            function calculation(id) {
+                var rate = parseFloat($('#rate_' + id).val()).toFixed(2);
+                var hours = parseFloat($('#hours_' + id).val()).toFixed(2);
+                var total = rate * hours;
+                var ytd_total = total * 52;
+                setTimeout(function() {
+                    $('#total_' + id).val(total);
+                    $('#period_' + id).val(total);
+                    $('#ytd_total_' + id).val(ytd_total);
+                    gross_total();
+                }, 300);
+            }
+
+            function gross_total() {
+                var total = 0;
+                $('.gross_total').each(function() {
+                    total += parseFloat(this.value);
+                });
+                $('.pay_start').change(function() {
+                    dayCalculate();
+                });
+
+                setTimeout(function() {
+                    $("#period_gross_total").val(total);
+                    $("#ytd_gross_total").val(ytd_total);
+                    default_tax();
+                }, 300);
+            }
+
+            function default_tax() {
+                var period_gross_total = $("#period_gross_total").val();
+                var ytd_gross_total = $("#ytd_gross_total").val();
+                console.log("period_gross_total", period_gross_total);
+                console.log("ytd_gross_total", ytd_gross_total);
+                $('.taxes').each(function() {
+                    var taxes_ids = $(this).data('id');
+                    var taxes_values = $(this).data('value');
+                    period_tax_price = period_gross_total * (taxes_values / 100);
+                    period_ytd_tax_price = ytd_gross_total * (taxes_values / 100);
+                    $('#taxes_' + taxes_ids).val(period_tax_price);
+                    $('#taxes_ytd_' + taxes_ids).val(period_ytd_tax_price);
+                });
+            }
         });
-
-        function calculation(id) {
-            var rate = parseFloat($('#rate_' + id).val()).toFixed(2);
-            var hours = parseFloat($('#hours_' + id).val()).toFixed(2);
-            var total = rate * hours;
-            var ytd_total = total * 52;
-            setTimeout(function() {
-                $('#total_' + id).val(total);
-                $('#period_' + id).val(total);
-                $('#ytd_total_' + id).val(ytd_total);
-                gross_total();
-            }, 300);
-        }
-
-        function gross_total() {
-            var total = 0;
-            $('.gross_total').each(function() {
-                total += parseFloat(this.value);
-            });
-            $('.pay_start').change(function() {
-                dayCalculate();
-            });
-
-            setTimeout(function() {
-                $("#period_gross_total").val(total);
-                $("#ytd_gross_total").val(ytd_total);
-                default_tax();
-            }, 300);
-        }
-
-        function default_tax() {
-            var period_gross_total = $("#period_gross_total").val();
-            var ytd_gross_total = $("#ytd_gross_total").val();
-            console.log("period_gross_total", period_gross_total);
-            console.log("ytd_gross_total", ytd_gross_total);
-            $('.taxes').each(function() {
-                var taxes_ids = $(this).data('id');
-                var taxes_values = $(this).data('value');
-                period_tax_price = period_gross_total * (taxes_values / 100);
-                period_ytd_tax_price = ytd_gross_total * (taxes_values / 100);
-                $('#taxes_' + taxes_ids).val(period_tax_price);
-                $('#taxes_ytd_' + taxes_ids).val(period_ytd_tax_price);
-            });
-        }
     });
 
 </script>
 
 <script>
     $(document).ready(function() {
-        var maxField = 12;
-        var addDeduction = $('.add_deduction');
-        var wrapper = $('#add_deduction');
-        var x = 1;
-        var i = 1;
-        $(addDeduction).click(function() {
-            var fieldHTML =
-                '<div class="row mb-3">' +
-                '<div class="col-md-3">' +
-                '<i class="fa fa-lock earnbtn2"></i>' +
-                '<input class="earnbtn text-center" type="text" value="">' +
-                '</div>' +
-                '<div class="col-md-1"> </div>' +
-                '<div class="col-md-3"> </div>' +
-                '<div class="col-md-1"> </div>' +
-                '<div class="col-md-2">' +
-                '<input type="number" step="0.01" class="earnbtn text-center tax_deduction tax" value=""/>' +
-                '</div>' +
-                '<div class="col-md-2">' +
-                '<input type="number" step="0.01" class="earnbtn text-center ytd_tax tax" value=""/>' +
-                '</div>' +
-                '</div>';
-            if (x < maxField) {
-                x++;
-                $(wrapper).append(fieldHTML);
-            }
-            $('.tax_deduction').keyup(function() {
-                var total = 0;
-                $('.tax_deduction').each(function() {
-                    total += parseFloat(this.value);
-                });
-                setTimeout(function() {
-                    $(".deduction_tax").val(total);
-                }, 300);
-
+    var maxField = 12;
+    var addDeduction = $('.add_deduction');
+    var wrapper = $('#add_deduction');
+    var x = 1;
+    var i = 1;
+    $(addDeduction).click(function() {
+        var fieldHTML =
+            '<div class="row mb-3">' +
+            '<div class="col-md-3">' +
+            '<i class="fa fa-lock earnbtn2"></i>' +
+            '<input class="earnbtn text-center" type="text" value="">' +
+            '</div>' +
+            '<div class="col-md-1"> </div>' +
+            '<div class="col-md-3"> </div>' +
+            '<div class="col-md-1"> </div>' +
+            '<div class="col-md-2">' +
+            '<input type="number" step="0.01" class="earnbtn text-center tax_deduction tax" value=""/>' +
+            '</div>' +
+            '<div class="col-md-2">' +
+            '<input type="number" step="0.01" class="earnbtn text-center ytd_tax tax" value=""/>' +
+            '</div>' +
+            '</div>';
+        if (x < maxField) {
+            x++;
+            $(wrapper).append(fieldHTML);
+        }
+        $('.tax_deduction').keyup(function() {
+            var total = 0;
+            $('.tax_deduction').each(function() {
+                total += parseFloat(this.value);
             });
+            setTimeout(function() {
+                $(".deduction_tax").val(total);
+            }, 300);
 
-                $('.ytd_tax').keyup(function() {
-                    var ytd_tax = 0;
-                    $('.ytd_tax').each(function() {
-                        ytd_tax += parseFloat(this.value);
-                    });
-                    $(".ytd_deduction_tax").val(ytd_tax);
-                });
-                setTimeout(function() {
-                    $(".ytd_deduction_tax").val(ytd_tax);
-                }, 300);
-
-            });
         });
+
+        $('.ytd_tax').keyup(function() {
+            var ytd_tax = 0;
+            $('.ytd_tax').each(function() {
+                ytd_tax += parseFloat(this.value);
+            });
+            $(".ytd_deduction_tax").val(ytd_tax);
+        });
+        setTimeout(function() {
+            $(".ytd_deduction_tax").val(ytd_tax);
+        }, 300);
+
+    });
+    });
     });
 
 </script>
@@ -721,6 +726,6 @@
 
 </script>
 
-        });
-    </script>
+});
+</script>
 @endsection
