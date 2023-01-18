570961419b2b17ea51b2180536fa9fc65745c33b
Mradul Sharma <mradul@possibilitysolutions.com>
2023-01-18 10:09:38 +0530

    getStateTaxes

--- app/Models/StateTax.php
+++ app/Models/StateTax.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class StateTax extends Model
 {
     use HasFactory;
+    static function getStateTaxes($request)
+    {
+        $dataObj = StateTax::get();
+        $response['data'] = $dataObj;
+        $response['status'] = STATUS_OK;
+        return $response;
+    }
 }
