a23d9c00c7232f52a411fbefe1d1a23f97aad71e
cepoch <cepochdevelopers@gmail.com>
2023-01-20 11:31:20 +0530

    sageblueukbasic template completed

--- app/Http/Controllers/TemplateFormController.php
+++ app/Http/Controllers/TemplateFormController.php
@@ -228,10 +228,29 @@ class TemplateFormController extends Controller
             'date' => date('m/d/Y')
         ];
           
-    //    return view('allForms.ukbasic-tawny', $data);
+        return view('allForms.ukbasic-tawny', $data);
 
         $pdf = PDF::loadView('allForms.ukbasic-tawny', $data);   
         return $pdf->stream('ukbasic-tawny.pdf');
+
+
+  
+    
+    
+     
+    }
+
+    public function BasicUkPDF()
+    {
+        $data = [
+            
+            'date' => date('m/d/Y')
+        ];
+          
+      // return view('allForms.sage-blue', $data);
+
+        $pdf = PDF::loadView('allForms.sage-blue', $data);   
+        return $pdf->stream('ukbasicsage.pdf');
 
 
   
