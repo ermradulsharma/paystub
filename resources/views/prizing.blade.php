e86d8523a8e30b77c115b9aeb642a7df693a1605
sanjeev <sansanjeev2012@gmail.com>
2023-01-20 17:39:56 +0530

    prizing page full design

--- resources/views/prizing.blade.php
+++ resources/views/prizing.blade.php
@@ -5,20 +5,20 @@
 
 <div>
 
-    <div class="container" style="max-width:1500px; border: 1px solid red; background: #E8E6E6;">
+    <div class="container" style="max-width:1420px; border: 1px solid red; background: #E8E6E6;">
         <div>
             <div class="row">
                 <div class=" col-lg-12 text-center p-1"
                     style="background: linear-gradient(to bottom, rgba(64,150,238,1) 0%,rgba(64,150,238,1) 48%,rgba(66,123,247,1) 50%,rgba(66,123,247,1) 99%);">
-                    <h2>Complete Your Order</h2>
+                    <h2 class="text-light">Complete Your Order</h2>
                 </div>
-                <div class="col-lg-12 text-center">
+                <div class="col-lg-12 text-center pb-5">
                     <p>Congratulations. Select your plan to gain unlimited access. Paystubs & Tax Forms</p>
                 </div>
             </div>
         </div>
 
-        <div class="section">
+        <div class="container mt-5">
             <div class="row">
                 <div class="col-lg-4 d-flex justify-content-center">
                     <div class="prizebox">
@@ -48,8 +48,8 @@
                 </div>
 
                 <div class="col-lg-4 d-flex justify-content-center">
-                    <div class="prizebox">
-                        <h3 class="my-5 pb-5 prizeh3">1 Month</h3>
+                    <div class="prizebox1">
+                        <h3 class="my-5 pb-5 prizeh3">3 Month</h3>
                         <div class="">
                             <div class="d-flex justify-content-center">
                                 <i class="fa fa-check priceicon"></i>
@@ -75,8 +75,8 @@
                 </div>
 
                 <div class="col-lg-4 d-flex justify-content-center">
-                    <div class="prizebox">
-                        <h3 class="my-5 pb-5 prizeh3">1 Month</h3>
+                    <div class="prizebox2">
+                        <h3 class="my-5 pb-5 prizeh3">6 Month</h3>
                         <div class="">
                             <div class="d-flex justify-content-center">
                                 <i class="fa fa-check priceicon"></i>
