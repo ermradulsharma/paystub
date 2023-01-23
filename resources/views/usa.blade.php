e37d6d229096b0215af8e643f016857d82055ccc
Mradul Sharma <mradul@possibilitysolutions.com>
2023-01-23 10:41:58 +0530

    close modal

--- resources/views/usa.blade.php
+++ resources/views/usa.blade.php
@@ -6,7 +6,7 @@
     <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
