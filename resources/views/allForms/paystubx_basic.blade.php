762d7d2b8a539010ebac10beb6c855fdb72d90ca
Mradul Sharma <mradul@possibilitysolutions.com>
2023-02-02 18:50:54 +0530

    templates

--- resources/views/allForms/paystubx_basic.blade.php
+++ resources/views/allForms/paystubx_basic.blade.php
@@ -6,29 +6,75 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Paystubx Template</title>
-
-
-
     <style>
-    .infomation {
-        border: 2px solid darkgrey;
-        border-top: none;
-    }
+        .infomation {
+            border: 2px solid darkgrey;
+            border-top: none;
+        }
+        .bodertop {
+            border: 1px solid black;
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            border: 2px solid darkgrey;
+            border-top: none;
+        }
+        .hiddden {
+            visibility: hidden;
+        }
+        .main {
+            display: inline-flexbox;
 
+        }
+        .section_2 {
+            padding-top: 5px;
+            background: #a9a9a9;
+            color: white;
+            height: 35px;
+        }
+        .earning {
+            text-align: right;
+            font-size: 20px;
+        }
+        .row::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
+        .col1 {
+            float: left;
+            width: 40%;
+        }
+        .col2 {
+            float: left;
+            width: 60%;
+        }
+        .row1::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
+        .tablewidth {
+            width: 100%;
+            text-align: center;
+        }
+        .column1 {
+            float: left;
+            width: 60%;
 
-    .bodertop {
-        border: 1px solid black;
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        border: 2px solid darkgrey;
-        border-top: none;
-    }
+        .column2 {
+            float: left;
+            width: 40%;
+        }
 
+        .hadding {
+            font-size: 12px;
+            text-transform: uppercase;
+        }
 
-    .hiddden {
-        visibility: hidden;
-    }
+        td {
+            font-size: 13px;
+        }
 
     .main {
         display: inline-flexbox;
@@ -112,11 +158,7 @@
         <table style="width: 100%;">
             <thead style="background-color: #a9a9a9;">
                 <th style="text-align:left;">#767767</th>
-
-                <th style="text-align:right; padding-right:20px; font-size:larger;">
-
-                    Earning Statement</th>
-
+                <th style="text-align:right; padding-right:20px; font-size:larger;"> Earning Statement </th>
             </thead>
         </table>
     </div>
@@ -125,22 +167,12 @@
         <div class="row">
             <div class="col1">
                 <table>
-
                     <tr>
                         <td style="font-size: 14px;"><b>{{ $requestData['cname'] }}</b></td>
-
-
                     </tr>
                     <tr>
-
-                        <td>
-                            {{ $requestData['address_1'] }},</br>
-                            {{ $requestData['city'] }} {{ $requestData['state'] }}, {{ $requestData['zip_code'] }}USA
-                        </td>
-
+                        <td>{{ $requestData['address_1'] }},</br>{{ $requestData['city'] }} {{ $requestData['state'] }}, {{ $requestData['zip_code'] }}, USA </td>
                     </tr>
-
-
                     <tr>
                         <td style="margin-top: 10px;"><span style="font-weight: 500;">Marital Status: </span>
                             {{ $requestData['marital_status'] }} </td>
@@ -149,19 +181,13 @@
                     </tr>
 
                     <tr>
-                        <td> <span style="font-weight: 500;">Exemptions: </span> {{ $requestData['exemptions'] }}
-                        </td>
-
-
+                        <td> <span style="font-weight: 500;">Exemptions: </span> {{ $requestData['exemptions'] }}</td>
                     </tr>
-
                 </table>
             </div>
 
             <div class="col2">
-
                 <table>
-
                     <tr>
                         <td> <span style="font-weight: 500;">Pay Period: <span>
                                     {{ date('m/d/y', strtotime($requestData['pay_start'])) }} -
@@ -179,38 +205,22 @@
 
 
                     </tr>
-
-
-
-
                     <tr>
-                        <td><b>Employee #: </b> <span> {{ $requestData['emp_id'] }}</span></td>
-
-
-
+                        <td><span style="font-weight: 500;">Employee #: </span> <span> {{ $requestData['emp_id'] }}</span></td>
                     </tr>
-
-
                     <tr>
-
                         <td>
                             {{ $requestData['emp_street_1'] }}, </br>
                             {{ $requestData['emp_city'] }}
-                            {{ $requestData['emp_state'] }},{{ $requestData['emp_zip_code'] }}USA
+                            {{ $requestData['emp_state'] }},{{ $requestData['emp_zip_code'] }}, USA
                         </td>
-
                     </tr>
 
                     <tr>
-                        <td><b>Social Security#:</b> <span>{{ $requestData['emp_ssn'] }}</span></td>
+                        <td><span style="font-weight: 500;">Social Security#:</span> <span>{{ $requestData['emp_ssn'] }}</span></td>
                     </tr>
-
                 </table>
             </div>
-
-
-
-
         </div>
     </section>
 
@@ -220,7 +230,6 @@
             <div class="column1">
                 <table class="tablewidth">
                     <thead>
-
                         <th class="hadding" style="text-align: left;">EARNINGS</th>
                         <th class="hadding">RATE</th>
                         <th class="hadding">HOURS</th>
@@ -229,32 +238,24 @@
 
                     </thead>
                     <tbody>
-
-
-                    @foreach ($requestData['earning'] as $key => $earn)
-                    <tr>
-                        <td style="text-align: left;">{{ $earn }}</td>
-                        <td>{{ $requestData['currency'] }} {{ $requestData['rate'][$key] }}</td>
-                        <td style="text-align:center;">{{ $requestData['hours'][$key] }}</td>
-                        <td>{{ $requestData['currency'] }} {{ $requestData['period'][$key] }}</td>
-                        <td>{{ $requestData['currency'] }} {{ $requestData['ytd_total'][$key] }}</td>
-                    </tr>
-                    @endforeach
+                        @foreach ($requestData['earning'] as $key => $earn)
+                        <tr>
+                            <td style="text-align: left;">{{ $earn }}</td>
+                            <td>{{ $requestData['currency'] }} {{ $requestData['rate'][$key] }}</td>
+                            <td style="text-align:center;">{{ $requestData['hours'][$key] }}</td>
+                            <td>{{ $requestData['currency'] }} {{ $requestData['period'][$key] }}</td>
+                            <td>{{ $requestData['currency'] }} {{ $requestData['ytd_total'][$key] }}</td>
+                        </tr>
+                        @endforeach
                     </tbody>
-
-
-
-
                 </table>
             </div>
             <div class="column2">
                 <table class="tablewidth">
                     <thead>
-
                         <th class="hadding" style="text-align: left;">DEDUCTIONS</th>
                         <th class="hadding">TOTAL</th>
                         <th class="hadding">YTD TOTAL</th>
-
                     </thead>
                     <tbody>
 
@@ -293,7 +294,7 @@
                 <td style="text-align: right;">{{ $requestData['currency'] }} {{ $requestData['deduction_tax'] }}</td>
                 <td style="text-align: right;">{{ $requestData['currency'] }} {{ $requestData['ytd_deduction_tax'] }}</td>
 
-                <th style="text-align: right;" >Net Pay</th>
+                <th style="text-align: right;">Net Pay</th>
                 <td style="text-align: right;">{{ $requestData['currency'] }} {{ $requestData['total_net_pay'] }}</td>
                 <td style="text-align: center;">{{ $requestData['currency'] }} {{ $requestData['total_ytd_net_pay'] }}</td>
             </tr>
