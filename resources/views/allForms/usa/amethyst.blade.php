37be74e796e4abc06b2d7a0ec179e3e066e902ac
Mradul Sharma <mradul@possibilitysolutions.com>
2023-02-09 18:06:59 +0530

    revert watermark

--- resources/views/allForms/usa/amethyst.blade.php
+++ resources/views/allForms/usa/amethyst.blade.php
@@ -158,7 +158,7 @@
         top: 50px;
         left: 0px;
         right: 0;
-        background-image: url("http://44.202.105.74/user/watermark.png");
+        background-image: url("http://44.202.105.74/user/water.png");
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center;
