1b1a0309e51eca6464468ffcf74643b9a457f211
Mradul Sharma <mradul@possibilitysolutions.com>
2023-02-17 15:50:54 +0530

    route

--- routes/web.php
+++ routes/web.php
@@ -103,6 +103,6 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('invoiceMail/{id}', [TemplateFormController::class, 'invoiceMail'])->name('invoiceMailId');
     Route::get('invoiceMail', [TemplateFormController::class, 'invoiceMail'])->name('invoiceMail');
     Route::get('invoiceEdit/{id}', [TemplateFormController::class, 'edit'])->name('invoiceEdit');
-    Route::get('prizing', [TemplateFormController::class, 'prizing'])->name('prizing');
+    Route::get('prizing', [PayStubController::class, 'prizing'])->name('prizing');
     Route::get('subscription', [TemplateFormController::class, 'subscription'])->name('subscription');
 });
