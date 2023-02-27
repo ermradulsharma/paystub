d1ee49d171d276ba93d28e2306ae3377897d2366
Mradul Sharma <mradul@possibilitysolutions.com>
2023-02-27 15:05:02 +0530

    dateformate

--- resources/views/usaPaystub.blade.php
+++ resources/views/usaPaystub.blade.php
@@ -411,8 +411,8 @@
                                         </label>
                                         <input
                                             style="color:#140303f5;border:1px solid #110303fe;padding:0px 6px !important; height:40px; appearance: none;"
-                                            type="date" id="pay_start" name="pay_start" placeholder="12-11-2022"
-                                            class="w-100 p-2 input-box-font pay_start datepicker" data-id="pay_start" value="<?php echo date('Y-m-d'); ?>">
+                                            type="text" id="pay_start" name="pay_start" placeholder="12-11-2022"
+                                            class="w-100 p-2 input-box-font pay_start datepicker inputdatepicker" data-id="pay_start" value="<?php echo date('m-d-Y'); ?>">
                                     </div>
                                 </div>
 
@@ -422,8 +422,8 @@
                                         </label>
                                         <input
                                             style="color:#140303f5;border:1px solid #110303fe; padding:0px 6px !important; height:40px; appearance: none;"
-                                            type="date" id="pay_end" name="pay_end" placeholder="12-17-2022"
-                                            class="w-100 p-2 input-box-font pay_end  datepicker" data-id="pay_end" value="<?php echo date('Y-m-d'); ?>">
+                                            type="text" id="pay_end" name="pay_end" placeholder="12-17-2022"
+                                            class="w-100 p-2 input-box-font pay_end datepicker inputdatepicker" data-id="pay_end" value="<?php echo date('m-d-Y'); ?>">
                                     </div>
                                 </div>
 
@@ -433,8 +433,8 @@
                                         </label>
                                         <input
                                             style="color:#140303f5;padding:0px 6px !important; height:40px; appearance: none; border:1px solid #110303fe;"
-                                            type="date" id="pay_date" name="pay_date" placeholder="12-19-2022"
-                                            class="w-100 p-2 input-box-font pay_date  datepicker" data-id="pay_date" value="<?php echo date('Y-m-d'); ?>">
+                                            type="text" id="pay_date" name="pay_date" placeholder="12-19-2022"
+                                            class="w-100 p-2 input-box-font pay_date datepicker inputdatepicker" data-id="pay_date" value="<?php echo date('m-d-Y'); ?>">
                                     </div>
                                 </div>
                                 <div class="col-md-3 mt-4">
