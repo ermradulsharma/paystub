1b6aec72ece44c92ef0121cad2a1fb4fc8b62ef1
Mradul Sharma <mradul@possibilitysolutions.com>
2023-03-02 19:54:53 +0530

    revert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -496,13 +496,13 @@
                 error: function(err) {
                     console.log('err', err);
                     error = err.responseJSON;
-                    error.errors.forEach((element, i)=> {
+                    /* error.errors.forEach((element, i)=> {
                         if(i == 0){
                             $('#'+element.key).focus();
                         }
                         $('#'+element.key).css('border-color', 'red');
                         $('#'+element.key).parent().parent().children("div").append('<div class="text-danger">'+element.message+'</div>');
-                    });
+                    }); */
 
                     toastr.error(error.message);
                     document.getElementById("loaderDiv").style.display = "none";
