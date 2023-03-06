aaeff641686edd8f2f86929e6e0fcffcd349a988
Mradul Sharma <mradul@possibilitysolutions.com>
2023-03-06 18:46:57 +0530

    img remove

--- public/user/js/calculations.js
+++ public/user/js/calculations.js
@@ -65,7 +65,6 @@ $(document).ready(function () {
     $(addDeduction).click(function () {
         var fieldHTML = '<div class="row">' +
             '<div class="col-md-4 col-lg-3 mb-3">' +
-            '<img src="http://paystubx.com/images/lock.png" class="earnbtn2">' +
             '<input name="tax_deduction[]" class="earnbtn text-center tax_deduction_0 tax_deduction_' + i + ' " data-id="' + i + '" type="text">' +
             "</div>" +
             '<div class="col-md-1 col-lg-1"> </div>' +
@@ -611,7 +610,8 @@ $(document).ready(function () {
 
     $('.lock').click(function () {
         var id = $(this).data('id');
-        unlockPadLock(id)
+        console.log('id', id);
+        unlockPadLock(id);
     });
 
     function unlockPadLock(id) {
