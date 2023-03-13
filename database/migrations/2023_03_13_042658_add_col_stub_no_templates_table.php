1fd7e844405a1ab679a3922673d86c3a6780cd6f
Mradul Sharma <mradul@possibilitysolutions.com>
2023-03-13 11:05:11 +0530

    usa stub no

---
+++ database/migrations/2023_03_13_042658_add_col_stub_no_templates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+            $table->string('stub_no')->default(0)->comment('0-true, 1-false');
+            $table->string('co_no')->default(0)->comment('0-true, 1-false');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('templates', function (Blueprint $table) {
+            //
+        });
+    }
+};
