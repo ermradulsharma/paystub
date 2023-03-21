2785884810aa9e07aff5272025a2ee4eb115115a
Mradul Sharma <mradul@possibilitysolutions.com>
2023-03-21 18:48:55 +0530

    orderBy

--- app/Http/Controllers/TemplateFormController.php
+++ app/Http/Controllers/TemplateFormController.php
@@ -27,8 +27,8 @@ class TemplateFormController extends Controller
     {
         $invoiceData = PaySlip::where(['user_id' => Auth::user()->id, 'id' => $id])->first() ?? [];
         $deduction = Deduction::where('state', $invoiceData->type)->orderBy('id', 'asc')->get();
-        $basicType = Template::where(['state' => $invoiceData->type, 'type' => 'basic', 'status' => 1])->with('images')->get();
-        $advanceType = Template::where(['state' => $invoiceData->type, 'type' => 'advance', 'status' => 1])->with('images')->get();
+        $basicType = Template::where(['state' => $invoiceData->type, 'type' => 'basic', 'status' => 1])->orderBy('title')->with('images')->get();
+        $advanceType = Template::where(['state' => $invoiceData->type, 'type' => 'advance', 'status' => 1])->orderBy('title')->with('images')->get();
         $stateTaxes = StateTax::get();
         $currencies = Currency::get();
         return view('lists/' . $invoiceData->type . '-edit', compact('basicType', 'advanceType', 'deduction', 'stateTaxes', 'currencies', 'invoiceData'));
