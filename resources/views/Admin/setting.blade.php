a226776fc101fb577955045e8e267cfd0b7fb00d
Mradul Sharma <mradul@possibilitysolutions.com>
2023-03-30 19:10:15 +0530

    setting

--- resources/views/Admin/setting.blade.php
+++ resources/views/Admin/setting.blade.php
@@ -6,15 +6,70 @@
             <h1>Settings</h1>
         </div><!-- End Page Title -->
 
-    <section class="section dashboard">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="row">
-                    <div class="col-lg-4">
-                        {{-- ******************************************************* SMTP ******************************************************* --}}
-                        <div class="card">
-                            <div class="card-header">
-                                <h4 class="d-flex align-items-center">SMTP</h4>
+        <section class="section dashboard">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            {{-- ******************************************************* SMTP ******************************************************* --}}
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="d-flex align-items-center">SMTP</h4>
+                                </div>
+
+                                <div class="card-body">
+                                    <form id="smtp" action="{{ route('settings') }}" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="request_type" value="smtp">
+                                        <div class="form-group">
+                                            <label>Email:</label>
+                                            <input type="email" name="smtp_email" class="form-control" placeholder="Email"
+                                                value="{{ $smtp['email'] ?? '' }}" autocomplete="off">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>Password:</label>
+                                            <input type="password" id="smtp-password" name="smtp_password"
+                                                class="form-control show-password-sd" placeholder="Password"
+                                                value="{{ $smtp['password'] ?? '' }}" autocomplete="off">
+                                            <div style="margin-top: 3px;">
+                                                <span>
+                                                    <input type="checkbox" class="show-password-checkbox">
+                                                </span>
+                                                <span style="margin-left: 3px;">Show Password</span>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>Mail Host:</label>
+                                            <input type="text" name="smtp_host" class="form-control"
+                                                placeholder="Mail Host" value="{{ $smtp['host'] ?? '' }}">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>Mail Port:</label>
+                                            <input type="text" name="smtp_port" class="form-control"
+                                                placeholder="Mail Port" value="{{ $smtp['port'] ?? '' }}">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>From Address:</label>
+                                            <input type="text" name="smtp_from_address" class="form-control"
+                                                placeholder="From Address" value="{{ $smtp['from_address'] ?? '' }}">
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label>From Name:</label>
+                                            <input type="text" name="smtp_from_name" class="form-control"
+                                                placeholder="From Name" value="{{ $smtp['from_name'] ?? '' }}">
+                                        </div>
+
+                                        <div class="text-center mt-5">
+                                            <button type="submit" class="btn btn-primary">Submit <i
+                                                    class="icon-paperplane ml-2"></i></button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
 
 
@@ -78,53 +133,56 @@
                                 </div>
                             </div>
                         </div>
-
-
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4 class="d-flex align-items-center">Paypal Configuration</h4>
-                            </div>
-                            <div class="card-body">
-                                <form id="paypal_configuration_form" action="{{ route('settings') }}" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="request_type" value="paypal_configuration">
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="paypal_mode" id="paypal-sandbox" value="SandBox" {{(isset($currencyData['paypal_mode']) && $currencyData['paypal_mode'] =='SandBox' ? 'checked' : '')}}>
-                                        <label class="form-check-label" for="paypal-sandbox">SandBox</label>
-                                    </div>
-                                    <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="paypal_mode" id="paypal-live" value="Live" {{(isset($currencyData['paypal_mode']) && $currencyData['paypal_mode'] =='Live' ? 'checked' : '')}}>
-                                        <label class="form-check-label" for="paypal-live">Production</label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Client ID:</label>
-                                        <input type="text" name="client_id" class="form-control" placeholder="Enter Client ID" value="{{($currencyData['client_id'] ?? '')}}">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Client Secret:</label>
-                                        <input type="text" name="client_secret" class="form-control" placeholder="Enter Client secret" value="{{($currencyData['client_secret'] ?? '')}}">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>App ID:</label>
-                                        <input type="text" name="app_id" class="form-control" placeholder="Enter App ID" value="{{($currencyData['app_id'] ?? '')}}">
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Currency:</label>
-                                        <select name="currency" id="currency_dropdown" class="form-control">
-                                            <option value="">Select Currency</option>
-                                            @foreach ($currencies as $key => $currency)
-                                                <option value="{{$currency}}" @if(isset($currencyData['currency']) && $currencyData['currency'] == $currency) selected @endif>{{$currency}}</option>
-                                            @endforeach
-                                        </select>
-
-                                    </div>
-                                    <div class="text-center mt-5">
-                                        <button type="submit" class="btn btn-primary">Submit <i class="icon-paperplane ml-2"></i></button>
-                                    </div>
-
-                                </form>
+                        <div class="col-lg-4">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="d-flex align-items-center">Change Password</h4>
+                                </div>
+                                <div class="card-body">
+                                    <form id="change-password-form" action="{{ route('settings') }}" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="request_type" value="change_password">
+                                        <div class="form-group">
+                                            <label>Current Password:</label>
+                                            <input type="password" name="old_password"
+                                                class="form-control show-password-sd" placeholder="Current Password"
+                                                required>
+                                            <div style="margin-top: 3px;">
+                                                <span>
+                                                    <input type="checkbox" class="show-password-checkbox">
+                                                </span>
+                                                <span style="margin-left: 3px;">Show Password</span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>New Password:</label>
+                                            <input type="password" id="password" name="password"
+                                                class="form-control show-password-sd" placeholder="New Password" required>
+                                            <div style="margin-top: 3px;">
+                                                <span>
+                                                    <input type="checkbox" class="show-password-checkbox">
+                                                </span>
+                                                <span style="margin-left: 3px;">Show Password</span>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Confirm Password:</label>
+                                            <input type="password" name="password_confirmation"
+                                                class="form-control show-password-sd" placeholder="Confirm Password"
+                                                required>
+                                            <div style="margin-top: 3px;">
+                                                <span>
+                                                    <input type="checkbox" class="show-password-checkbox">
+                                                </span>
+                                                <span style="margin-left: 3px;">Show Password</span>
+                                            </div>
+                                        </div>
+                                        <div class="text-center mt-5">
+                                            <button type="submit" class="btn btn-primary">Submit <i
+                                                    class="icon-paperplane ml-2"></i></button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                         <div class="col-lg-4">
