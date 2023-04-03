d6504588b37525adae84759e2a36adf4f54413ee
Mradul Sharma <mradul@possibilitysolutions.com>
2023-04-03 17:37:09 +0530

    address_2

--- resources/views/allForms/usa/pt_brown.blade.php
+++ resources/views/allForms/usa/pt_brown.blade.php
@@ -163,7 +163,8 @@
                     <h6 style="font-size: 15px; margin-bottom: 0;text-transform:capitalize;"> {{ $requestData['cname'] }}
                     </h6>
                     <p style="font-size: 14px; margin: 0;text-transform:uppercase;"> {{ $requestData['address_1']
-                        }}<br>{{ $requestData['address_2'] }}<br>{{ $requestData['city'] }}, {{ $requestData['state'] }} {{ $requestData['zip_code'] }}
+                        }}@if($requestData['address_2'] != '')<br>
+                        {{ $requestData['address_2'] }} @endif<br>{{ $requestData['city'] }}, {{ $requestData['state'] }} {{ $requestData['zip_code'] }}
                     </p>
                 </div>
                 <div style="width:40%;float:right;">
