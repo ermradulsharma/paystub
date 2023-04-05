36208eac4e5123599bdbc322e29ca7f7f6f305ee
cepoch <cepochdevelopers@gmail.com>
2023-04-05 15:48:24 +0530

    new template

--- resources/views/mail/verificationcode.blade.php
+++ resources/views/mail/verificationcode.blade.php
@@ -1,134 +1,89 @@
-<!DOCTYPE html>
-<html lang="en">
+
+<!doctype html>
+<html lang="en-US">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        @import url('https://fonts.cdnfonts.com/css/myriad-pro');
-
-        @font-face {
-            font-family: 'Myriad Pro', sans-serif;
-        }
-
-        body {
-            font-family: 'Myriad Pro', sans-serif;
-
-        }
-
-        .email-verification {
-            padding: 10px;
-            background-color: #eaeaea;
-
-        }
-
-        .logo {
-            width: 100%;
-            max-width: 80px;
-            padding: 20px 70px;
-            border: 2px solid #f26e20;
-            background-color: #fff;
-        }
-
-        .footer-section {
-            padding-top: 15px;
-            background-color: #eaeaea;
-        }
-
-        table {
-            width: 100%;
-        }
-
-        ul li {
-            list-style: none;
-        }
-
-        ul.social-icons {
-            padding: 0px 0px 20px;
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-        }
-
-        ul.social-icons li {
-            margin-right: 10px;
-        }
-
-        ul.social-icons li:last-child {
-            margin-right: 0;
-        }
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>New Account Email Template</title>
+    <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+    <meta name="description" content="New Account Email Template.">
+    <style type="text/css">
+        a:hover {text-decoration: underline !important;}
     </style>
 </head>
-<body>
-    <main style=" max-width: 500px;margin:50px auto;background-color:#4472c4;padding:35px 20px;">
-        <section class="email-verification">
-            <table style="background-color:#fff;padding:10px 20px;">
-                <tr>
-                    <td style="text-align: center; position: relative; bottom:60px;"><img class="logo"
-                            src="images/Paystub X.webp"></td>
-                </tr>
-                <tr>
-                    <td style="text-align: center"><img src="images/laptop.png"></td>
-                </tr>
-                <tr>
-                    <td style="color:#676767;font-weight:bold;padding-bottom:15px; padding-top:10px;font-size:15px;">Your
-                        Paystubx E-Mail Verification</td>
-                </tr>
-                <tr>
-                    <td style="font-size: 12px; color:rgba(86, 86,86,0.9); padding-bottom:15px;">Your one step closing to
-                        accessing Paystub Acccount! Please enter the code below to confirm your email
-                        address.</td>
-                </tr>
-                <tr>
-                    <td style="letter-spacing: 4px; text-align:center;padding-bottom:15px;"><span
-                            style="border:1px solid rgba(86, 86,86,0.6); ;padding:3px 6px;">226542</span></td>
 
-                </tr>
-                <tr>
-                    <td style="color: rgba(86, 86,86,0.9); text-align:center;padding-bottom:20px;">This code expires in 24
-                        hours.</td>
-                </tr>
-                <tr>
-                    <td style="font-size: 12px; color:rgba(86, 86,86,0.9); padding-bottom:15px;">Use the verification code
-                        provided to start using your paystubx account.if to did not make this request, please ignore this
-                        email.</td>
-                </tr>
-                <tr>
-                    <td style="color:rgba(86, 86,86,0.9); font-size:14px; ">Welcome,</td>
-                </tr>
-                <tr>
-                    <td style="font-size: 14px;color:rgba(86, 86,86,0.9);"><b style="color: #000;">Paystub<span
-                                style="color: red;">X</span></b> Support</td>
-                </tr>
-                <tr>
-                    <td style="font-size: 14px; color:#4472c4;">Support@Paystubx.com</td>
-                </tr>
-            </table>
-        </section>
-        <section class="footer-section">
-            <div class="footer-content">
-                <p style="text-align: center;padding:0;">Create Pay stubs on the go!</p>
-            </div>
-            <div class="row" style="display: flex; justify-content:center;">
-                <img style="max-width: 100px; height:30px; margin-right:10px;" src="images/1app.png">
-                <img style="max-width: 100px; height:30px;" src="images/1google.png">
-            </div>
-            <div class="footer-content">
-                <p style="text-align: center">Follow us on social media</p>
-            </div>
-            <ul class="social-icons">
-                <li><a><img src="images/icons/fb.png"></a></li>
-                <li><a><img src="images/icons/in.png"></a></li>
-                <li><a><img src="images/icons/instagram.png"></a></li>
-                <li><a><img src="images/icons/pin.png"></a></li>
-                <li><a><img src="images/icons/twitter.png"></a></li>
-                <li><a><img src="images/icons/youtube.png"></a></li>
-                </div>
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    <!-- 100% body table -->
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+        style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td>
+                <table style="background-color: #f2f3f8; max-width:670px; margin:0 auto;" width="100%" border="0"
+                    align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <a href="https://rakeshmandal.com" title="logo" target="_blank">
+                            <img width="60" src="https://i.ibb.co/hL4XZp2/android-chrome-192x192.png" title="logo" alt="logo">
+                          </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                style="max-width:670px; background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Get started
+                                        </h1>
+                                        <p style="font-size:15px; color:#455056; margin:8px 0 0; line-height:24px;">
+                                            Your account has been created on the e-Health Care Admin HTML Theme by Propeller Pro. Below are your system generated credentials, <br><strong>Please change
+                                                the password immediately after login</strong>.</p>
+                                        <span
+                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                        <p
+                                            style="color:#455056; font-size:18px;line-height:20px; margin:0; font-weight: 500;">
+                                            <strong
+                                                style="display: block;font-size: 13px; margin: 0 0 4px; color:rgba(0,0,0,.64); font-weight:normal;">Username</strong>wendell@xyz.com
+                                            <strong
+                                                style="display: block; font-size: 13px; margin: 24px 0 4px 0; font-weight:normal; color:rgba(0,0,0,.64);">Password</strong>f1_M1@j3[I2~
+                                        </p>
 
-        </section>
-    </main>
+                                        <a href="login.html"
+                                            style="background:#20e277;text-decoration:none !important; display:inline-block; font-weight:500; margin-top:24px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Login
+                                            to your Account</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:center;">
+                            <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.rakeshmandal.com</strong> </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="height:80px;">&nbsp;</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <!--/100% body table-->
 </body>
+
 </html>
-
-
