af34bd1772a3655cff9c6af3d985dfb88c412071
Mradul Sharma <15956546+ermradulsharma@users.noreply.github.com>
2023-04-06 11:10:21 +0530

    verify

--- resources/views/mail/verify.blade.php
+++ resources/views/mail/verify.blade.php
@@ -33,10 +33,7 @@
     <table style="background-color: #f2f3f8; max-width:670px; margin:30px auto;" width="100%" align="center"
         cellpadding="0" cellspacing="0">
         <tr>
-            <td style="text-align:center;padding-bottom:10px;">
-                <a style="" target="_blank"><img width="230px"
-                        src="{{asset('images/Paystub X.webp')}}" title="logo" alt="logo"></a>
-            </td>
+            <td style="text-align:center;padding-bottom:10px;"><a style="" target="_blank"><img width="230px" src="{{asset('images/Paystub X.webp')}}" title="logo" alt="logo"></a></td>
         </tr>
         <tr>
             <td style="height:15px;">&nbsp;</td>
@@ -47,12 +44,9 @@
                     style="max-width:670px; background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
                     <tr>
                         <td style="padding:0 20px;">
-                            <p style="text-align: center;"><img style="text-align: center;padding-top:15px;"
-                                    src="{{asset(" images/laptop.png")}}"></p>
-                            <h3 style="text-align: left;color:#686767; font-size:15px;">Your Paystubx
-                                E-Mail Verification</h3>
-                            <p
-                                style="font-size:13px; color:rgba(86, 86,86,0.9); margin:8px 0 0; line-height:24px;text-align:left;padding-bottom:25px;">
+                            <p style="text-align: center;"><img style="text-align: center;padding-top:15px;" src="{{asset("images/laptop.png")}}"></p>
+                            <h3 style="text-align: left;color:#686767; font-size:15px;">Your Paystubx E-Mail Verification</h3>
+                            <p style="font-size:13px; color:rgba(86, 86,86,0.9); margin:8px 0 0; line-height:24px;text-align:left;padding-bottom:25px;">
                                 Your one step closing to
                                 accessing Paystub Acccount! Please enter the code below to confirm your
                                 email
@@ -60,15 +54,9 @@
                             </p>
 
 
-                            <p
-                                style="letter-spacing: 10px; padding:4px 8px;border:1px solid rgba(86, 86,86,0.5);width:120px; margin:0 auto;">
-                                {{$user_data['otp']}}
+                            <p style="letter-spacing: 10px; padding:4px 8px;border:1px solid rgba(86, 86,86,0.5);width:120px; margin:0 auto;">{{$user_data['otp']}}</p>
 
-                            </p>
-
-                            <p style="text-align: center;color:rgba(86, 86,86,0.9);font-size:13px;">This code expires
-                                in 24
-                                hours.</p>
+                            <p style="text-align: center;color:rgba(86, 86,86,0.9);font-size:13px;">This code expires in 24 hours.</p>
                             <p
                                 style="font-size:13px; color:rgba(86, 86,86,0.9); margin:8px 0 0; line-height:24px;text-align:left;padding-bottom:25px;">
                                 Use the verification code
@@ -106,21 +94,12 @@
                 </p>
                 <p style="text-align: center;color:rgba(86, 86,86,0.9);font-size:13px;">Follow us on social media</p>
                 <ul class="social-icons" style="width:48%; margin:0 auto;">
-                    <li style="float: left;margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;"
-                                src="{{asset('images/icons/fb.png')}}"></a>
-                    </li>
-                    <li style="float: left;margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;"
-                                src="{{asset('images/icons/linkedin.png')}}"></a>
-                    </li>
-                    <li style="float: left;margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;"
-                                src="{{asset('images/icons/instagram.png')}}"></a></li>
-                    <li style="float: left;margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;"
-                                src="{{asset('images/icons/pintrest.png')}}"></a>
-                    </li>
-                    <li style="float: left;margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;"
-                                src="{{asset('images/icons/twitter.png')}}"></a></li>
-                    <li style="float: left;"><a href="https://www.google.com/"><img style="width: 30px;"
-                                src="{{asset('images/icons/youtube.png')}}"></a></li>
+                    <li style="float: left; margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;" src="{{asset('images/icons/fb.png')}}"></a></li>
+                    <li style="float: left; margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;" src="{{asset('images/icons/linkedin.png')}}"></a></li>
+                    <li style="float: left; margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;" src="{{asset('images/icons/instagram.png')}}"></a></li>
+                    <li style="float: left; margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;" src="{{asset('images/icons/pintrest.png')}}"></a></li>
+                    <li style="float: left; margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;" src="{{asset('images/icons/twitter.png')}}"></a></li>
+                    <li style="float: left; margin-right:6px;"><a href="https://www.google.com/"><img style="width: 30px;" src="{{asset('images/icons/youtube.png')}}"></a></li>
                 </ul>
             </td>
         </tr>
