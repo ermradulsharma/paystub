4998b52d38627da68efae21751ab0114a4ac75a2
Sandeep <sandeep.cepoch@gmail.com>
2023-04-07 20:36:36 +0530

    login issue fixed

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -155,7 +155,9 @@ class LoginController extends Controller
             //User::where('email', request('email'))->where('is_completed', '0')->delete();
             $user = new User;
             $user->email = $request->email;
+            $user->is_completed = '0';
         }
+
         if($user->is_completed == '0'){
             if ($user->email != "") {
                 $mailData = [];
