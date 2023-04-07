476e7aa413ac4b1b05a70692f70eef9f1bc530e5
cepoch <possibilitysolution2020@gmail.com>
2023-04-07 10:29:26 +0530

    CHANGE

--- resources/views/mail/verify.blade.php
+++ resources/views/mail/verify.blade.php
@@ -55,7 +55,7 @@
                                     E-Mail Verification</h3>
                                 <p
                                     style="font-size:13px; color:rgba(86, 86,86,0.9); margin:8px 0 0; line-height:24px;text-align:left;padding-bottom:25px;">
-                                    You're one step closing to
+                                    You are one step closing to
                                     accessing Paystub Acccount! Please enter the code below to confirm your
                                     email
                                     address.
@@ -74,7 +74,7 @@
                                 <p
                                     style="font-size:13px; color:rgba(86, 86,86,0.9); margin:8px 0 0; line-height:24px;text-align:left;padding-bottom:25px;">
                                     Use the verification code
-                                    provided to start using your paystubx account. If to did not make this
+                                    provided to start using your paystubx account. If You did not make this
                                     request, please ignore this
                                     email.
                                 </p>
