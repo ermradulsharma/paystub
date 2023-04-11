ea64d4b826b71c6372fea4aa8e957d61f2aaddde
Mradul Sharma <mradul@possibilitysolutions.com>
2023-04-11 13:00:35 +0530

    watermark

--- app/Http/Controllers/API/TemplatesController.php
+++ app/Http/Controllers/API/TemplatesController.php
@@ -51,7 +51,6 @@ class TemplatesController extends Controller
         try {
             if ($requestData['form_type'] == "w2form") {
                 $pageName = "w2form";
-                $requestData['watermark'] = 'yes';
             } else {
                 if ($requestData['advance_temp']) {
                     $pageName = $requestData['advance_temp'];
