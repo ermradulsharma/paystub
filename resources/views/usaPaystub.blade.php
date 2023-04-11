981ac606e536241f404cae414312e7e9b449d6de
Mradul Sharma <mradul@possibilitysolutions.com>
2023-04-11 17:06:54 +0530

    check number

--- resources/views/usaPaystub.blade.php
+++ resources/views/usaPaystub.blade.php
@@ -806,16 +806,6 @@
                             $(".stubxc").addClass("col-md-3");
                         });
                     }
-
-                    if (stub == 0 && check == 0) {
-                        $('.stubxc').each(function() {
-                            $(".stubxc").addClass("col-md-4");
-                            $(".stubxcv").addClass("d-none");
-                            $(".checkxcv").addClass("d-none");
-                            $(".stubxc").removeClass("col-md-3");
-                        });
-                    }
-
                     if (status == 1) {
                         $(".tempElemant").removeClass("d-none");
                         var clock = $('option:selected', '.at_id').data('clock');
@@ -880,7 +870,7 @@
                     $('.stubxc').each(function() {
                         $(".stubxc").addClass("col-md-4");
                         $(".stubxcv").addClass("d-none");
-                        $(".stubxcv").addClass("d-none");
+                        $(".checkxcv").addClass("d-none");
                         $(".stubxc").removeClass("col-md-3");
                     });
                     $(".tempElemant").addClass("d-none");
