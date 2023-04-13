9caa563dfc03708efcc4d431c131217d1123344f
Mradul Sharma <mradul@possibilitysolutions.com>
2023-04-13 17:38:44 +0530

    google login

--- public/user/js/main.js
+++ public/user/js/main.js
@@ -146,7 +146,7 @@ function handleCredentialResponse(response) {
         }).join(''));
         return JSON.parse(jsonPayload);
     }
-    log
+
     const responsePayload = decodeJwtResponse(response.credential);
     $.ajax({
         url: baseUrl + "google/callback",
