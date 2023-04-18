d092edfbdf15d1af35820655ade875afc79792ee
Mradul Sharma <mradul@possibilitysolutions.com>
2023-04-18 19:36:05 +0530

    remove comma

--- resources/views/allForms/usa/aegean.blade.php
+++ resources/views/allForms/usa/aegean.blade.php
@@ -196,7 +196,7 @@
                             <p style="font-size:14px;font-weight:400; font-family: 'Arial', sans-serif;position: relative; bottom:20px;">SSN: XXX-XX-{{ $requestData['emp_ssn'] }}</p>
                             <p style="padding: 0; margin:0;font-weight:400; font-size:14px; font-family: 'Arial', sans-serif;position: relative; top:25px;"> Stub No: {{ $requestData['stub_no'] }}</p>
                         </td>
-                        <td class="earning" style="width: 60%;font-weight:400 !important;padding-bottom:0px !important; padding-top:0px !important; margin:0px; font-size:16px; font-family: Arial, Helvetica, sans-serif; color:#f7f0f9;text-transform:capitalize; ">{{ $requestData['emp_name'] }} <br>Emp.ID. {{ $requestData['emp_id'] }} <br> {{ $requestData['emp_street_1'] }},<br>{{$requestData['emp_street_2'] }},<br>{{ $requestData['emp_city'] }}, {{ $requestData['emp_state'] }} {{ $requestData['emp_zip_code'] }}</td>
+                        <td class="earning" style="width: 60%;font-weight:400 !important;padding-bottom:0px !important; padding-top:0px !important; margin:0px; font-size:16px; font-family: Arial, Helvetica, sans-serif; color:#f7f0f9;text-transform:capitalize; ">{{ $requestData['emp_name'] }} <br>Emp.ID. {{ $requestData['emp_id'] }} <br> {{ $requestData['emp_street_1'] }}<br>{{$requestData['emp_street_2'] }}<br>{{ $requestData['emp_city'] }}, {{ $requestData['emp_state'] }} {{ $requestData['emp_zip_code'] }}</td>
                     </tr>
                 </table>
                 @else
