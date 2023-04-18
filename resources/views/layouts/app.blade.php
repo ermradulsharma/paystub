4ece732edcfc818bc4630b68a3d21b8b754ff29c
Mradul Sharma <mradul@possibilitysolutions.com>
2023-04-18 15:10:52 +0530

    Change Google API

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -176,7 +176,7 @@
                 <div class="modal-body" style="padding-bottom:30px;">
                     <div class="google-btn mt-4" style="text-align: -webkit-center; text-align: -moz-center;">
                         <div id="g_id_onload"
-                            data-client_id="802702825376-57405b5o70d0l41mkh9q8ta86ig71rkb.apps.googleusercontent.com"
+                            data-client_id="601945306946-q0b39bgi5kd1ub68jp1sm27h1335mgg6.apps.googleusercontent.com"
                             data-callback="handleCredentialResponse" data-dismiss="modal" data-ux_mode="popup"
                             data-auto_prompt="false">
                         </div>
