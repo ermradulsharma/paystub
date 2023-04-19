68d62ba298a626c70a09a9deecc3ae7cf8893f77
Mradul Sharma <mradul@possibilitysolutions.com>
2023-04-19 15:27:37 +0530

    templates

--- resources/views/allForms/usa/paystubx_check.blade.php
+++ resources/views/allForms/usa/paystubx_check.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>paystubs-check</title>
+    <title>Paystub Check</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200&display=swap');
