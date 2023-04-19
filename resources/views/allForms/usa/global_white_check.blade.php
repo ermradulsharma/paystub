163ada9267509bcd3bd34a2bf064d5e23d1827d0
Mradul Sharma <15956546+ermradulsharma@users.noreply.github.com>
2023-04-19 19:00:22 +0530

    end

--- resources/views/allForms/usa/global_white_check.blade.php
+++ resources/views/allForms/usa/global_white_check.blade.php
@@ -384,7 +384,7 @@
                 $earn = count($requestData['earning']);
                 $tax = count($requestData['tax_deduction']);
             @endphp
-            <div class="container" style=" margin-top:100px; width:100%; padding:0px 20px; @if($earn != 1 || $tax != 0) position: fixed;  top:76%; @else position: relative;  top:64px; @endif">
+            <div class="container" style=" margin-top:100px; width:100%; padding:0px 20px; @if($earn != 1) position: fixed;  top:76%; @elseif($tax != 0)position: fixed;  top:76%; @else position: relative;  top:64px; @endif">
                 <div class="row" style="display: flex;justify-content: space-between;padding: 0px 14px;">
                     <div class="global-address" style="width: 50%; float:left; position: relative; @if($requestData['address_2'] != '') bottom:40px; @else bottom:10px; @endif right:0px; left:60px;">
                         <p style="font-size:15px; margin: 0;color:black;font-family: Arial, Helvetica, sans-serif; text-transform:uppercase;font-weight:400;">{{ $requestData['cname'] }}</p>
