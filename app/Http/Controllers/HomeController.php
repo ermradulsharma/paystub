018fda6612f6092b91573c9bbdf50126bf398c54
cepoch <cepochdevelopers@gmail.com>
2023-04-25 16:28:41 +0530

    contacus

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
@@ -253,6 +253,8 @@ class HomeController extends Controller
 
     public function contactForm(Request $request)
     {
+
+
         $response = [];
         $response['success'] = FALSE;
         $response['status'] = STATUS_BAD_REQUEST;
@@ -262,8 +264,8 @@ class HomeController extends Controller
             $mailData['email'] = $request->email;
             $mailData['message'] = $request->w3review;;
             $mailData['subject'] = 'Contact Form';
-            // Mail::to('paystubxlogger@gmail.com')->send(new ContactForm($mailData));
-            Mail::to('tripti@yopmail.com')->send(new ContactForm($mailData));
+            Mail::to('paystubxlogger@gmail.com')->send(new ContactForm($mailData));
+            //Mail::to('rajiv@yopmail.com')->send(new ContactForm($mailData));
 
             $response['success'] = TRUE;
             $response['message'] = "Your account setup successfully.";
